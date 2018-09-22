472883c262192243c7ffee0a1699ddd8f4982ad6
Juannito Gonzalez <juannito@gmail.com>
2018-09-22 11:57:27 -0300

    mostrando mapa

--- components/Map.js
+++ components/Map.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
 import { MapView } from 'expo';
 
+import { StyleSheet} from "react-native"
+
 export default class Map extends Component {
+
   render() {
     return (
       <MapView
-        style={{ flex: 1 }}
+        style={StyleSheet.absoluteFill}
         initialRegion={{
           latitude: 37.78825,
           longitude: -122.4324,
